0392d77ad3cbc36af6b7e21d1302eefbd75f9566
Dodai-Me <ukokododai69@gmail.com>
2025-09-03 01:38:27 +0100

    Second Page Commenced!

--- lib/components/second_page.dart
+++ lib/components/second_page.dart
@@ -5,6 +5,6 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('I am the second page');
+    return ListView(children: []);
   }
 }
